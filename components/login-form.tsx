f94da508e6bc2411621e411638d1657a9efcfe66
Cass-1 <cass.dahle@gmail.com>
2025-03-17 15:57:41 -0700

    adds mouse hover on login button

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -7,9 +7,9 @@ export default function LoginForm() {
       "use server";
       await signIn("github", {redirectTo: "/"});
     }}
-    className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+    className="flex cursor-pointer items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
   >
-    <button type="submit">Log In</button>
+    <button className="cursor-pointer" type="submit">Log In</button>
   </form>
   );
 }
